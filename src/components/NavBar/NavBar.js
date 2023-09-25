d77c2c3f208725b394a13521b8cab18ae45dadfb
gionata <barale.gionata@gmail.com>
2023-09-25 16:25:31 +0200

    navbar responsive

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import './NavBar.css';
 import logo from '../../assets/main_flower.png';
 import { Link } from 'react-scroll';
-import menu from '../../assets/menu.png';
+import MenuIcon from '@mui/icons-material/Menu';
 import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
 
 const NavBar = () => {
@@ -24,13 +24,13 @@ const NavBar = () => {
                 <ChatBubbleOutlineIcon className='chat' /><span>Contact Me</span>
             </button>
 
-            <img alt='Menu' src={menu} className='mobile-menu' onClick={() => setShowMenu(!showMenu)} />
+            <MenuIcon id='mobile-menu' onClick={() => setShowMenu(!showMenu)} />
             <div className='mobile-menu-list' style={{ display: showMenu ? 'flex' : 'none' }}>
-                <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-40} duration={500} className='mobile-menu-list-item' onClick={() => setShowMenu(false)}>Home</Link>
-                <Link activeClass='active' to='skills' spy={true} smooth={true} offset={-40} duration={500} className='mobile-menu-list-item' onClick={() => setShowMenu(false)}>About</Link>
-                <Link activeClass='active' to='soft-skills' spy={true} smooth={true} offset={-40} duration={500} className='mobile-menu-list-item' onClick={() => setShowMenu(false)}>Soft</Link>
-                <Link activeClass='active' to='works' spy={true} smooth={true} offset={-40} duration={500} className='mobile-menu-list-item' onClick={() => setShowMenu(false)}>Portfolio</Link>
-                <Link activeClass='active' to='contact' spy={true} smooth={true} offset={-40} duration={500} className='mobile-menu-list-item' onClick={() => setShowMenu(false)}>Contact</Link>
+                <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-40} duration={500} className='item' onClick={() => setShowMenu(false)}>Home</Link>
+                <Link activeClass='active' to='skills' spy={true} smooth={true} offset={-40} duration={500} className='item' onClick={() => setShowMenu(false)}>About</Link>
+                <Link activeClass='active' to='soft-skills' spy={true} smooth={true} offset={-40} duration={500} className='item' onClick={() => setShowMenu(false)}>Soft</Link>
+                <Link activeClass='active' to='works' spy={true} smooth={true} offset={-40} duration={500} className='item' onClick={() => setShowMenu(false)}>Portfolio</Link>
+                <Link activeClass='active' to='contact' spy={true} smooth={true} offset={-40} duration={500} className='item' onClick={() => setShowMenu(false)}>Contact</Link>
             </div>
         </nav>
     )
