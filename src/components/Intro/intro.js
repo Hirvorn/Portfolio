88e911d46be4d9f469b63d21c827f052b0315da6
gionata <barale.gionata@gmail.com>
2023-09-30 17:14:26 +0200

    small screens adjustments

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -7,7 +7,7 @@ const Intro = () => {
             <Wrapper />
             <div className='intro-content'>
                 <div className='hello'>Hello,</div>
-                <div className='intro-text'>I'm <span className='intro-name'>Gionata Barale</span> <br />Senior Android Developer</div>
+                <div className='intro-text'>I'm <br className='carriage' /><span className='intro-name'>Gionata Barale</span> <br />Senior Android Developer</div>
                 <div className='intro-secondary-text'>and React JS Dev</div>
             </div>
         </section>
