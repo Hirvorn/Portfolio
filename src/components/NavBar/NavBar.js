70d87302a24543c0aa885c09515d5d60783de7fd
gionata <barale.gionata@gmail.com>
2023-10-04 19:50:30 +0200

    add noSelect

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -12,11 +12,11 @@ const NavBar = () => {
         <nav className='navbar'>
             <img alt='Logo' src={logo} className='logo' />
             <div className='desktop-menu'>
-                <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-40} duration={500} className='list-item'>Home</Link>
-                <Link activeClass='active' to='skills' spy={true} smooth={true} offset={0} duration={500} className='list-item'>About</Link>
-                <Link activeClass='active' to='soft-skills' spy={true} smooth={true} offset={0} duration={500} className='list-item'>Soft</Link>
-                <Link activeClass='active' to='works' spy={true} smooth={true} offset={0} duration={500} className='list-item'>Portfolio</Link>
-                <Link activeClass='active' to='contact' spy={true} smooth={true} offset={0} duration={500} className='list-item'>Contact</Link>
+                <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-40} duration={500} className='list-item noSelect'>Home</Link>
+                <Link activeClass='active' to='skills' spy={true} smooth={true} offset={0} duration={500} className='list-item noSelect'>About</Link>
+                <Link activeClass='active' to='soft-skills' spy={true} smooth={true} offset={0} duration={500} className='list-item noSelect'>Soft</Link>
+                <Link activeClass='active' to='works' spy={true} smooth={true} offset={0} duration={500} className='list-item noSelect'>Portfolio</Link>
+                <Link activeClass='active' to='contact' spy={true} smooth={true} offset={0} duration={500} className='list-item noSelect'>Contact</Link>
             </div>
             <button className='contact-me' onClick={() => {
                 document.getElementById('contact').scrollIntoView({ behavior: 'smooth' });
@@ -24,15 +24,15 @@ const NavBar = () => {
                 <ChatBubbleOutlineIcon className='chat' /><span>Contact Me</span>
             </button>
 
-            <span className='mobile-menu'>
+            <span className='mobile-menu noSelect'>
                 <Hamburger className='mobile-menu' toggled={showMenu} onToggle={toggled => { setShowMenu(toggled) }} rounded />
             </span>
             <div className='mobile-menu-list' style={{ visibility: showMenu ? 'visible' : 'hidden', pointerEvents: showMenu ? 'all' : 'none', opacity: showMenu ? '1' : '0' }}>
-                <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-40} duration={500} className='item' onClick={() => setShowMenu(false)}>Home</Link>
-                <Link activeClass='active' to='skills' spy={true} smooth={true} offset={-40} duration={500} className='item' onClick={() => setShowMenu(false)}>About</Link>
-                <Link activeClass='active' to='soft-skills' spy={true} smooth={true} offset={-40} duration={500} className='item' onClick={() => setShowMenu(false)}>Soft</Link>
-                <Link activeClass='active' to='works' spy={true} smooth={true} offset={-40} duration={500} className='item' onClick={() => setShowMenu(false)}>Portfolio</Link>
-                <Link activeClass='active' to='contact' spy={true} smooth={true} offset={-40} duration={500} className='item' onClick={() => setShowMenu(false)}>Contact</Link>
+                <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-40} duration={500} className='item noSelect' onClick={() => setShowMenu(false)}>Home</Link>
+                <Link activeClass='active' to='skills' spy={true} smooth={true} offset={-40} duration={500} className='item noSelect' onClick={() => setShowMenu(false)}>About</Link>
+                <Link activeClass='active' to='soft-skills' spy={true} smooth={true} offset={-40} duration={500} className='item noSelect' onClick={() => setShowMenu(false)}>Soft</Link>
+                <Link activeClass='active' to='works' spy={true} smooth={true} offset={-40} duration={500} className='item noSelect' onClick={() => setShowMenu(false)}>Portfolio</Link>
+                <Link activeClass='active' to='contact' spy={true} smooth={true} offset={-40} duration={500} className='item noSelect' onClick={() => setShowMenu(false)}>Contact</Link>
             </div>
         </nav>
     )
